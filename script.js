621675d37d42cb3cb267a9880e835ced5434d1df
Sasha5026 <skrstovic@DESKTOP-DK6OLV5.>
2025-04-06 15:12:22 +0200

    Write the logic to play a single round.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello world");
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let cChoice = Math.random();
+    if (cChoice <= 1/3) return 'Rock';
+    else if (1/3 < cChoice <= 2/3) return 'Paper';
+    else return 'Scissors';
+}
+
+function getHumanChoice() {
+    let hChoice = prompt("Rock, paper, scissoooors");
+    return hChoice;
+}
+
+console.log(getHumanChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
