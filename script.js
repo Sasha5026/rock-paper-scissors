94e86ff45dc6e61ef669c4544d857172299e1173
Sasha5026 <skrstovic@DESKTOP-DK6OLV5.>
2025-04-06 15:46:07 +0200

    Write the logic to play the entire game.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getComputerChoice() {
     let cChoice = Math.random();
     if (cChoice <= 1/3) return 'Rock';
     else if (1/3 < cChoice <= 2/3) return 'Paper';
-    else return 'Scissors';
+    else if (2/3 < cChoice) return 'Scissors';
 }
 
 function getHumanChoice() {
@@ -13,7 +13,30 @@ function getHumanChoice() {
     return hChoice;
 }
 
-console.log(getHumanChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+    let humChoice = humanChoice.toLowerCase();
+    let compChoice= computerChoice.toLowerCase();
+    switch(compChoice) {
+        case 'rock':
+            if (humChoice === 'rock') console.log("It's a tie! Let's go for another round!");
+            else if (humChoice === 'paper') console.log("You win! Paper beats rock!");
+            else if (humChoice === 'scissors') console.log("You lose! Rock beats scissors!");
+            break;
+        case 'paper':
+            if (humChoice === 'rock') console.log("You lose! Paper beats rock!");
+            else if (humChoice === 'paper') console.log("It's a tie! Let's go for another round!");
+            else if (humChoice === 'scissors') console.log("You win! Scissors beat paper!");
+            break;
+        case 'scissors':
+            if (humChoice === 'rock') console.log("You win! Rock beats scissors!");
+            else if (humChoice === 'paper') console.log("You lose! Scissors beat paper!");
+            else if (humChoice === 'scissors') console.log("It's a tie! Let's go for another round!");
+            break;
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
